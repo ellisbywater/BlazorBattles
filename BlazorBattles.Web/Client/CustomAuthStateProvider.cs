a92ca7ef93c78adb14f235b8b64ad4f5065b6f51
Ellis G <egaraudy@gmail.com>
2020-11-10 17:14:25 -0700

    create CustomAuthProvider

---
+++ BlazorBattles.Web/Client/CustomAuthStateProvider.cs
@@ -0,0 +1,33 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Web.Client
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorageService;
+
+        public CustomAuthStateProvider(ILocalStorageService localStorageService)
+        {
+            _localStorageService = localStorageService;
+        }
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            /* return Task.FromResult(new AuthenticationState(new ClaimsPrincipal()));*/
+            var identity = new ClaimsIdentity(
+                new[]
+                {
+                    new Claim(ClaimTypes.Name, "Ellis")
+                }, "test a claim");
+
+            var user = new ClaimsPrincipal(identity);
+
+            return Task.FromResult(new AuthenticationState(user));
+        }
+    }
+}
---
+++ BlazorBattles.Web/Shared/UserRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlazorBattles.Web.Shared
+{
+    public class UserRegistration
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [StringLength(16, ErrorMessage="Too long")]
+        public string Username { get; set; }
+        public string Bio { get; set; }
+        [Required, StringLength(100, MinimumLength = 8)]
+        public string Password { get; set; }
+        [Compare("Password", ErrorMessage ="Passwords must match")]
+        public string ConfirmPassword { get; set; }
+        [Range(300, 1000, ErrorMessage = "Choose a number between 300 & 1000")]
+        public int Bananas { get; set; } = 300;
+        public string StartUnitId { get; set; } = "1";
+        public DateTime DateOfBirth { get; set; } = DateTime.Now;
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Please Confirm")]
+        public bool IsConfirmed { get; set; } = true;
+    }
+}
